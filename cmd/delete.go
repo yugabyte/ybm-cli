655b4c412052310a4b02104c86ce5f1c3c85c6e3
Catalin David <cdavid@yugabyte.com>
2022-09-22 02:47:05 +0300

    update delete comments

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,13 +12,8 @@ import (
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Delete a resource in YugabyteDB Managed",
+	Long:  "Delete a resource in YugabyteDB Managed",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("delete called")
 	},
