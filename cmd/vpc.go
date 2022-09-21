cf68c1d60756b3c6e5a645fb734fd861b1f0bf6a
Catalin David <cdavid@yugabyte.com>
2022-09-22 02:45:41 +0300

    Complete VPC implementation for get/create/delete

--- cmd/vpc.go
+++ cmd/vpc.go
@@ -1,40 +1,171 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	openapi "github.com/yugabyte/yugabytedb-managed-go-client-internal"
 )
 
 // vpcCmd represents the vpc command
-var vpcCmd = &cobra.Command{
+var getVpcCmd = &cobra.Command{
 	Use:   "vpc",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Get VPCs in YugabyteDB Managed",
+	Long:  "Get VPCs in YugabyteDB Managed",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("vpc called")
+
+		apiClient, _ := getApiClient(context.Background())
+		accountID, _, _ := getAccountID(context.Background(), apiClient)
+		projectID, _, _ := getProjectID(context.Background(), apiClient, accountID)
+		vpcListRequest := apiClient.NetworkApi.ListSingleTenantVpcs(context.Background(), accountID, projectID)
+
+		// if user filters by name, add it to the request
+		vpcName, _ := cmd.Flags().GetString("name")
+		if vpcName != "" {
+			vpcListRequest = vpcListRequest.Name(vpcName)
+		}
+		resp, r, err := vpcListRequest.Execute()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `NetworkApi.ListVpcs``: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		}
+		// response from `ListClusters`: ClusterListResponse
+		prettyPrintJson(resp)
+	},
+}
+
+var createRegions []string
+var createCidrs []string
+var createVpcCmd = &cobra.Command{
+	Use:   "vpc",
+	Short: "Create a VPC in YugabyteDB Managed",
+	Long:  "Create a VPC in YugabyteDB Managed",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Validations
+		if !cmd.Flags().Changed("global-cidr") && !cmd.Flags().Changed("cidr") {
+			fmt.Fprintln(os.Stderr, "Either global-cidr or cidr must be specified")
+			os.Exit(1)
+		}
+
+		if len(createRegions) != len(createCidrs) {
+			fmt.Fprintln(os.Stderr, "Number of regions and cidrs must be equal")
+			os.Exit(1)
+		}
+
+		vpcName, _ := cmd.Flags().GetString("name")
+		cloud, _ := cmd.Flags().GetString("cloud")
+		globalCidrRange, _ := cmd.Flags().GetString("global-cidr")
+
+		// global CIDR only works with GCP
+		if cloud != "GCP" && cmd.Flags().Changed("global-cidr") {
+			fmt.Fprintln(os.Stderr, "global-cidr is only supported for GCP")
+			os.Exit(1)
+		}
+
+		// If non-global CIDR, validate that there are different regions specified
+		regionMap := map[string]int{}
+		vpcRegionSpec := []openapi.VpcRegionSpec{}
+
+		if cmd.Flags().Changed("cidr") {
+			for index, region := range createRegions {
+				cidr := createCidrs[index]
+				spec := *openapi.NewVpcRegionSpecWithDefaults()
+				regionMap[region] = index
+				spec.SetRegion(region)
+				spec.SetCidr(cidr)
+				vpcRegionSpec = append(vpcRegionSpec, spec)
+			}
+			if len(regionMap) != len(createRegions) {
+				fmt.Fprintln(os.Stderr, "Regions must be unique")
+				os.Exit(1)
+			}
+		}
+
+		vpcSpec := *openapi.NewSingleTenantVpcSpec(vpcName, openapi.CloudEnum(cloud), vpcRegionSpec)
+		if cmd.Flags().Changed("global-cidr") {
+			vpcSpec.SetParentCidr(globalCidrRange)
+		}
+		vpcRequest := *openapi.NewSingleTenantVpcRequest(vpcSpec)
+
+		// fmt.Fprintln(os.Stderr, "vpcName ", vpcName)
+		// fmt.Fprintln(os.Stderr, "cloud ", openapi.CloudEnum(cloud))
+		// fmt.Fprintln(os.Stderr, "globalCidrRange ", globalCidrRange)
+		// fmt.Fprintln(os.Stderr, "createRegions ", createRegions)
+		// fmt.Fprintln(os.Stderr, "createCIDRs ", createCidrs)
+		// fmt.Fprintln(os.Stderr, "vpcRequest ", vpcRequest)
+
+		// os.Exit(1)
+
+		apiClient, _ := getApiClient(context.Background())
+		accountID, _, _ := getAccountID(context.Background(), apiClient)
+		projectID, _, _ := getProjectID(context.Background(), apiClient, accountID)
+		resp, response, err := apiClient.NetworkApi.CreateVpc(context.Background(), accountID, projectID).SingleTenantVpcRequest(vpcRequest).Execute()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `NetworkApi.CreateVpc``: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", response)
+			return
+		}
+
+		prettyPrintJson(resp)
+	},
+}
+
+var deleteVpcCmd = &cobra.Command{
+	Use:   "vpc",
+	Short: "Delete a VPC in YugabyteDB Managed",
+	Long:  "Delete a VPC in YugabyteDB Managed",
+	Run: func(cmd *cobra.Command, args []string) {
+		vpcName, _ := cmd.Flags().GetString("name")
+		apiClient, _ := getApiClient(context.Background())
+		accountID, _, _ := getAccountID(context.Background(), apiClient)
+		projectID, _, _ := getProjectID(context.Background(), apiClient, accountID)
+
+		readResp, readResponse, readErr := apiClient.NetworkApi.ListSingleTenantVpcs(context.Background(), accountID, projectID).Name(vpcName).Execute()
+		if readErr != nil {
+			fmt.Fprintf(os.Stderr, "Unable to find VPC with name %v. Error when calling `NetworkApi.ListSingleTenantVpcs``: %v\n", vpcName, readErr)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", readResponse)
+			return
+		}
+		respData := readResp.Data
+		if len(respData) == 0 {
+			fmt.Fprintf(os.Stderr, "Unable to find VPC with name %v. Error when calling `NetworkApi.ListSingleTenantVpcs``: %v\n", vpcName, readErr)
+			return
+		}
+		vpcId := respData[0].Info.Id
+
+		resp, err := apiClient.NetworkApi.DeleteVpc(context.Background(), accountID, projectID, vpcId).Execute()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `NetworkApi.DeleteVpc``: %v\n", err)
+			return
+		}
+
+		fmt.Fprintf(os.Stdout, "VPC %v was queued for termination.\n", vpcName)
+		prettyPrintJson(resp)
 	},
 }
 
 func init() {
-	createCmd.AddCommand(vpcCmd)
+	getCmd.AddCommand(getVpcCmd)
+	getVpcCmd.Flags().String("name", "", "Name for the VPC")
 
-	// Here you will define your flags and configuration settings.
+	createCmd.AddCommand(createVpcCmd)
+	createVpcCmd.Flags().String("name", "", "Name for the VPC")
+	createVpcCmd.MarkFlagRequired("name")
+	createVpcCmd.Flags().String("cloud", "", "Cloud provider for the VPC")
+	createVpcCmd.MarkFlagRequired("cloud")
+	createVpcCmd.Flags().String("global-cidr", "", "Global CIDR for the VPC")
+	createVpcCmd.Flags().StringSliceVar(&createRegions, "region", []string{}, "")
+	createVpcCmd.Flags().StringSliceVar(&createCidrs, "cidr", []string{}, "")
+	createVpcCmd.MarkFlagsRequiredTogether("region", "cidr")
+	createVpcCmd.MarkFlagsMutuallyExclusive("global-cidr", "cidr")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// vpcCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// vpcCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	deleteCmd.AddCommand(deleteVpcCmd)
+	deleteVpcCmd.Flags().String("name", "", "Name for the VPC")
+	deleteVpcCmd.MarkFlagRequired("name")
 }
