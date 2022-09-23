f429028bb0e102b9c18ed2d613a80067b3a7820d
Srinivas Pothuraju <posriniv@gmail.com>
2022-09-23 15:51:08 -0500

    support pause and resume clusters (#9)
    * cluster creation final
    
    * validate map input
    
    * support update cluster
    
    * support pausing cluster
    
    * support resume cluster
    
    Former-commit-id: d3ad1d8db51f8782e050172730d72338e4ee7653

--- cmd/resume_cluster.go
+++ cmd/resume_cluster.go
@@ -11,11 +11,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// deleteClusterCmd represents the cluster command
-var deleteClusterCmd = &cobra.Command{
+// resumeClusterCmd represents the cluster command
+var resumeClusterCmd = &cobra.Command{
 	Use:   "cluster",
-	Short: "Delete cluster in YugabyteDB Managed",
-	Long:  "Delete cluster in YugabyteDB Managed",
+	Short: "Pause clusters in YugabyteDB Managed",
+	Long:  "Pause clusters in YugabyteDB Managed",
 	Run: func(cmd *cobra.Command, args []string) {
 		apiClient, _ := getApiClient(context.Background())
 		accountID, _, _ := getAccountID(context.Background(), apiClient)
@@ -26,29 +26,29 @@ var deleteClusterCmd = &cobra.Command{
 			fmt.Fprintf(os.Stderr, "Error when fetching cluster ID: %v\n", errMsg)
 			return
 		}
-		r, err := apiClient.ClusterApi.DeleteCluster(context.Background(), accountID, projectID, clusterID).Execute()
+
+		_, _, err := apiClient.ClusterApi.ResumeCluster(context.Background(), accountID, projectID, clusterID).Execute()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error when calling `ClusterApi.ListClusters``: %v\n", err)
-			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+			fmt.Fprintf(os.Stderr, "Error while resuming the cluster %v\n", clusterName)
 			return
 		}
 
-		fmt.Fprintf(os.Stdout, "The cluster %v is scheduled for deletion\n", clusterName)
-
+		fmt.Fprintf(os.Stdout, "The cluster %v is being resumed\n", clusterName)
 	},
 }
 
 func init() {
-	deleteCmd.AddCommand(deleteClusterCmd)
+	resumeCmd.AddCommand(resumeClusterCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// getClusterCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// resumeClusterCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	deleteClusterCmd.Flags().String("cluster-name", "", "The name of the cluster to be deleted")
-	deleteClusterCmd.MarkFlagRequired("cluster-name")
+	// resumeClusterCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	resumeClusterCmd.Flags().String("cluster-name", "", "The name of the cluster to be resumed")
+	resumeClusterCmd.MarkFlagRequired("cluster-name")
 }
