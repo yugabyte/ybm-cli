00fe07d5750f642cd4fd4564c08d0a9a500ffe1a
Srinivas Pothuraju <posriniv@gmail.com>
2022-10-18 15:51:54 -0500

    Create greet.go

---
+++ cmd/greet.go
@@ -0,0 +1,31 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+  
+)
+
+// greetCmd represents the greet command
+var greetCmd = &cobra.Command{
+	Use:   "greet",
+	Short: "Greet the users of YBM CLI",
+	Long:  "Greet the users of YBM CLI",
+	Run: func(cmd *cobra.Command, args []string) {
+		resp, r, err := apiClient.GreetingsApi.GetGreetings(context.Background()).Execute()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `GreetingsApi.GetGreetings`: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		}
+    prettyPrintJson(resp)
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+
+
+}
