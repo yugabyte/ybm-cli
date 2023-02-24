e706b090e7b84b3d74c06486be50b3cc2fc8fd64
Catalin David <cdavid@yugabyte.com>
2023-02-24 08:07:57 -0800

    Add SPDX identifiers to all of the source code.

--- internal/client/client.go
+++ internal/client/client.go
@@ -11,6 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2022- Yugabyte, Inc.
 
 package client
 
