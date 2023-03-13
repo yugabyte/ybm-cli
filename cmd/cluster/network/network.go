fdd9080ced24cc4a3e5d649aa90ee4ecce6c8bba
Catalin David <cdavid@yugabyte.com>
2023-03-13 14:54:21 -0700

    wip

--- cmd/cluster/network/network.go
+++ cmd/cluster/network/network.go
@@ -13,34 +13,22 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package cluster
+package network
 
 import (
 	"github.com/spf13/cobra"
-	"github.com/yugabyte/ybm-cli/cmd/cluster/network"
 )
 
-// getCmd represents the list command
-var ClusterCmd = &cobra.Command{
-	Use:   "cluster",
-	Short: "Manage cluster operations",
-	Long:  "Manage cluster operations",
+var NetworkCmd = &cobra.Command{
+	Use:   "network",
+	Short: "Manage network operations for a cluster",
+	Long:  "Manage network operations for a cluster",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
 }
 
 func init() {
-	ClusterCmd.AddCommand()
-	ClusterCmd.AddCommand(network.NetworkCmd)
+	NetworkCmd.AddCommand()
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
