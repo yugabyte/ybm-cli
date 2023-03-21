af203d59ae5a03d54d26dbb8b594105df07a3b9a
Srinivas Pothuraju <posriniv@gmail.com>
2023-03-21 20:48:42 +0530

    format the string better

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.1.0
+VERSION=0.0.1
 BINARY=ybm
 export GOPRIVATE := github.com/yugabyte
 default: build
--- cmd/root.go
+++ cmd/root.go
@@ -16,6 +16,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
@@ -32,6 +33,7 @@ import (
 	"github.com/yugabyte/ybm-cli/cmd/util"
 	"github.com/yugabyte/ybm-cli/cmd/vpc"
 	ybmAuthClient "github.com/yugabyte/ybm-cli/internal/client"
+	"github.com/yugabyte/ybm-cli/internal/formatter"
 	"github.com/yugabyte/ybm-cli/internal/log"
 	"github.com/yugabyte/ybm-cli/internal/releases"
 	"golang.org/x/mod/semver"
@@ -61,7 +63,8 @@ var rootCmd = &cobra.Command{
 		latestVersion, _ := releases.GetLatestRelease()
 		currentVersion := ybmAuthClient.GetVersion()
 		if semver.Compare(currentVersion, latestVersion) == -1 {
-			logrus.Printf("A newer version is available. Please upgrade to the latest version %s\n", latestVersion)
+			message := fmt.Sprintf("A newer version is available. Please upgrade to the latest version %s\n", latestVersion)
+			logrus.Println(formatter.Colorize(message, formatter.GREEN_COLOR))
 		}
 	},
 }
