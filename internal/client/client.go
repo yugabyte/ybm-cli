31b1642fc4b5371550e5c4e16ce7a0fede32b161
Etourneau Gwenn <getourneau@yugabyte.com>
2023-07-21 15:07:52 +0900

    Hide flag bhind FF (#169)

--- internal/client/client.go
+++ internal/client/client.go
@@ -253,9 +253,11 @@ func (a *AuthApiClient) CreateClusterSpec(cmd *cobra.Command, regionInfoList []m
 		faultTolerance, _ := cmd.Flags().GetString("fault-tolerance")
 		clusterInfo.SetFaultTolerance(ybmclient.ClusterFaultTolerance(faultTolerance))
 	}
-	if util.IsFeatureFlagEnabled(util.ENTERPRISE_SECURITY) && cmd.Flags().Changed("enterprise-security") {
-		enterpriseSecurity, _ := cmd.Flags().GetBool("enterprise-security")
-		clusterInfo.SetEnterpriseSecurity(enterpriseSecurity)
+	if util.IsFeatureFlagEnabled(util.ENTERPRISE_SECURITY) {
+		if cmd.Flags().Changed("enterprise-security") {
+			enterpriseSecurity, _ := cmd.Flags().GetBool("enterprise-security")
+			clusterInfo.SetEnterpriseSecurity(enterpriseSecurity)
+		}
 	}
 	clusterInfo.SetIsProduction(isProduction)
 	clusterInfo.SetNodeInfo(*ybmclient.NewClusterNodeInfoWithDefaults())
