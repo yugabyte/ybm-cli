ee53e01c8ea64e3a313281c32e9e81ae1f18399e
Aditya Kaushik <adityakaushik99@gmail.com>
2023-08-23 17:00:28 +0530

    WIP

--- cmd/metrics_exporter/metrics_exporter.go
+++ cmd/metrics_exporter/metrics_exporter.go
@@ -1,9 +1,27 @@
+// Licensed to Yugabyte, Inc. under one or more contributor license
+// agreements. See the NOTICE file distributed with this work for
+// additional information regarding copyright ownership. Yugabyte
+// licenses this file to you under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package metrics_exporter
 
 import (
+	"fmt"
+
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	ybmAuthClient "github.com/yugabyte/ybm-cli/internal/client"
+	"github.com/yugabyte/ybm-cli/internal/formatter"
 	ybmclient "github.com/yugabyte/yugabytedb-managed-go-client-internal"
 )
 
@@ -23,7 +41,7 @@ var createMetricsExporterCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		authApi, err := ybmAuthClient.NewAuthApiClient()
 		if err != nil {
-			logrus.Fatalf(ybmAuthClient.GetApiErrorDetails(err))
+			logrus.Fatalf("could not initiate api client: %s", err.Error())
 		}
 		authApi.GetInfo("", "")
 
@@ -32,15 +50,13 @@ var createMetricsExporterCmd = &cobra.Command{
 		datadogSpecString, _ := cmd.Flags().GetStringToString("datadog-spec")
 
 		metricsSinkTypeEnum, err := ybmclient.NewMetricsExporterConfigTypeEnumFromValue(metricsSinkType)
-		if err != nil {
-			logrus.Fatalf(err.Error())
-		}
 
 		apiKey := datadogSpecString["api-key"]
 		site := datadogSpecString["site"]
 
 		datadogSpec := ybmclient.NewDatadogMetricsExporterConfigurationSpec(apiKey, site)
 		metricsExporterConfigSpec := ybmclient.NewMetricsExporterConfigurationSpec(metricsExporterName, *metricsSinkTypeEnum)
+
 		metricsExporterConfigSpec.SetDatadogSpec(*datadogSpec)
 
 		resp, r, err := authApi.CreateMetricsExporterConfig().MetricsExporterConfigurationSpec(*metricsExporterConfigSpec).Execute()
@@ -50,7 +66,21 @@ var createMetricsExporterCmd = &cobra.Command{
 			logrus.Fatalf(ybmAuthClient.GetApiErrorDetails(err))
 		}
 
-		resp.GetData()
+		metricsExporterId := resp.GetData().Info.Id
+
+		msg := fmt.Sprintf("The metrics exporter config %s is being created", formatter.Colorize(metricsExporterId, formatter.GREEN_COLOR))
+
+		fmt.Println(msg)
 
 	},
+}
+
+func init() {
+	MetricsExporterCmd.AddCommand(createMetricsExporterCmd)
+
+	createMetricsExporterCmd.Flags().String("name", "", "[REQUIRED] The name of the cluster.")
+	createMetricsExporterCmd.MarkFlagRequired("name")
+	createMetricsExporterCmd.Flags().String("type", "", "[REQUIRED] The type of third party metrics sink")
+	createMetricsExporterCmd.MarkFlagRequired("type")
+	createMetricsExporterCmd.Flags().StringToString("datadog-spec", nil, "Spec for datadog")
 }
